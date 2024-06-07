9bacab824b92482530837ecc0f0bfbdaeecead84
viral parmar <parmarviral327@gmail.com>
2024-06-07 19:22:57 +0530

    add script

--- backend/built/constant/order_status.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.OrderStatus = void 0;
-var OrderStatus;
-(function (OrderStatus) {
-    OrderStatus["NEW"] = "NEW";
-})(OrderStatus || (exports.OrderStatus = OrderStatus = {}));
